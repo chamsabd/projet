67292c7258ce599e46ae142ae02cee70bbfe5fd8
ChouroukAmara <chouroukamara114@gmail.com>
2022-04-08 12:35:32 +0200

    create migrations -mfs

---
+++ laravel/database/migrations/2022_04_08_074320_create_departements_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDepartementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('departements', function (Blueprint $table) {
+            $table->id('id_dep');
+            $table->string('nom_dep');
+            $table->integer('id_admin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('departements');
+    }
+}
