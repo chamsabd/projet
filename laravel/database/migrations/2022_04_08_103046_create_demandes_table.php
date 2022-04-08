0bd6cd54f99e94647bab8ae5cc573683b0a8ea6f
nourhaine1 <ayadinourhaine@gmail.com>
2022-04-08 17:12:20 +0200

    Creating Models

---
+++ laravel/database/migrations/2022_04_08_103046_create_demandes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDemandesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('demandes', function (Blueprint $table) {
+            $table->id('id_demande');
+            $table->integer('id_responsable');
+            
+            $table->integer('id_client');
+            $table->integer('id_formation');
+            $table->dateTime('date_demande');
+            $table->timestamps();
+        });
+    }
+   
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('demandes');
+    }
+}
---
+++ laravel/database/migrations/2022_04_08_103726_create_etudiants_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEtudiantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('etudiants', function (Blueprint $table) {
+            $table->id();
+            $table->integer('carte_etud');
+            $table->integer('id_client'); 
+            $table->integer('id_responsable'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('etudiants');
+    }
+}
