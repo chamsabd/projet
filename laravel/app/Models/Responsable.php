f0f5d080f8c4cde72d235323c1fe0e781e642600
welhazitakwa <welhazitakwa@gmail.com>
2022-04-08 22:36:31 +0100

    relationsships de Responsable

--- laravel/app/Models/Responsable.php
+++ laravel/app/Models/Responsable.php
@@ -11,4 +11,26 @@ class Responsable extends Model
     protected $primaryKey='id_responsable';
    // public $incrementing = false ;
   // public $timestamps = false ;
+
+  public function etudiants()
+  {
+      return $this->hasMany('app\Models\Etudiant', 'id_responsable');
+  }
+
+  public function professeurs()
+  {
+      return $this->hasMany('app\Models\Professeur','id_responsable');
+  }
+
+  public function demandes()
+  {
+      return $this->hasMany('app\Models\Demande','id_responsable');
+  }
+
+  public function admin()
+  {
+      return $this->belongsTo('app\Admin','id_responsable');
+  }
+
+
 }
