3bf38d4e744baf924eff9aa6ac158e70d0aabe62
welhazitakwa <welhazitakwa@gmail.com>
2022-04-08 22:37:06 +0100

    relationships de seance

--- laravel/app/Models/Seance.php
+++ laravel/app/Models/Seance.php
@@ -10,4 +10,20 @@ class Seance extends Model
     use HasFactory;
     protected $primaryKey='id_seance';
     //public $timestamps = false ;
+    public function ressources()
+    {
+        return $this->hasMany('app\Models\Ressource','id_seance');
+    }
+
+    public function professeur()
+    {
+        return $this->belongsTo('app\Category','id_seance');
+    }
+
+    public function formation()
+    {
+        return $this->belongsTo('app\Formation','id_seance');
+    }
+
+
 }
