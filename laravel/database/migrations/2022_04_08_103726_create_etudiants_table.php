e997a225fef652de29d75968a64230ccbdb18874
nourhaine1 <ayadinourhaine@gmail.com>
2022-04-08 17:28:23 +0200

    .

--- laravel/database/migrations/2022_04_08_103726_create_etudiants_table.php
+++ laravel/database/migrations/2022_04_08_103726_create_etudiants_table.php
@@ -14,8 +14,8 @@ class CreateEtudiantsTable extends Migration
     public function up()
     {
         Schema::create('etudiants', function (Blueprint $table) {
-            $table->id();
-            $table->integer('carte_etud');
+           
+            $table->integer('carte_etud')->primary();
             $table->integer('id_client'); 
             $table->integer('id_responsable'); 
             $table->timestamps();
