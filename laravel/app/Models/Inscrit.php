a676cb5eb9633914a4dddea27e0b9702fd60aa8c
safedhaouadi <safadhaouadi319@gmail.com>
2022-04-10 00:12:03 +0100

    update inscrit factory

--- laravel/app/Models/Inscrit.php
+++ laravel/app/Models/Inscrit.php
@@ -11,15 +11,12 @@ class Inscrit extends Model
 
 public function Formation()
 {
-<<<<<<< HEAD
-    return $this->belongsTom(Formation::class,'id_formation');
-}    
-=======
+
     return $this->belongsTo(Formation::class,'id_formation');
 }  
 public function client()
 {
     return $this->belongsTo(Client::class,'id_formation');
 }  
->>>>>>> 47ca99f5f87940008a6a74ab616ca2b8a32fe0a7
+
 }
--- laravel/database/migrations/2022_04_08_103509_create_inscrits_table.php
+++ laravel/database/migrations/2022_04_08_103509_create_inscrits_table.php
@@ -15,19 +15,10 @@ class CreateInscritsTable extends Migration
     {
         Schema::create('inscrits', function (Blueprint $table) {
             $table->id();
-<<<<<<< HEAD:laravel/database/migrations/2022_04_08_102528_create_inscrits_table.php
-            $table->unsignedInteger('id_clients');
-            $table->unsignedInteger('id_formation');
-            $table->date('date_inscrit'); 
-          // $table->primary(['id_clients', 'id_formation', 'date_inscrit']);
-           // $table->foreign('id_clients')->references('id_clients')->on('clients')->onDelete('cascade')->onUpdate('cascade');
-=======
             $table->unsignedBigInteger('id_clients');
             $table->unsignedInteger('id_formation');
-            $table->date('date_inscrit'); 
-           $table->unique(['id_clients', 'id_formation', 'date_inscrit']);
+            $table->date('date_inscrit')->unique(); 
             $table->foreign('id_clients')->references('id_clients')->on('clients')->onDelete('cascade')->onUpdate('cascade');
->>>>>>> 47ca99f5f87940008a6a74ab616ca2b8a32fe0a7:laravel/database/migrations/2022_04_08_103509_create_inscrits_table.php
             $table->foreign('id_formation')->references('id_formation')->on('formations')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
 
