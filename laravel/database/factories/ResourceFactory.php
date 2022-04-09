257c5e4cbe857e8606136dda93e5354340e6b3bf
chamsabd <chamsabdelwahed42@gmail.com>
2022-04-09 19:48:57 +0100

    resource freign key

--- laravel/database/factories/ResourceFactory.php
+++ laravel/database/factories/ResourceFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Seance;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ResourceFactory extends Factory
@@ -14,14 +15,10 @@ class ResourceFactory extends Factory
     public function definition()
     {
         return [
-            // $table->string('url');
-           //// $table->string('type');
-           // $table->string('id_seance');
-           // $table->foreign('id_seance')->references('id_seance')->on('seances')->onDelete('cascade')->onUpdate('cascade');
-         'url'=>$this->faker->file($sourceDir = 'public', $targetDir = 'storage\img'),
+            'url'=>$this->faker->file($sourceDir = 'public', $targetDir = 'storage\img'),
            'type'=>$this->faker->fileExtension(),
-           'id_seance'=>$this->faker->numberBetween(10, 500),
-           //'id_seance'=>Seance::get('id_seance')->random(),
+          // 'id_seance'=>$this->faker->numberBetween(10, 500),
+           'id_seance'=>Seance::get('id_seance')->random(),
            'created_at'=>now()
         ];
     }
