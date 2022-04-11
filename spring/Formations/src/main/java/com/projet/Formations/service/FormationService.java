f69eaf418db4ab6ea8396f8b44f2f9ba7e5d5580
chamsabd <chamsabdelwahed42@gmail.com>
2022-04-11 20:54:52 +0100

    preparer Formation.service

--- spring/Formations/src/main/java/com/projet/Formations/service/FormationService.java
+++ spring/Formations/src/main/java/com/projet/Formations/service/FormationService.java
@@ -1,14 +1,19 @@
-package com.projet.Formations.dao;
+package com.projet.Formations.service;
 
 import java.util.Date;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
+
 
 import com.projet.Formations.entities.Formation;
 
-public interface FormationRepository extends JpaRepository<Formation, Long> { 
+public interface FormationService {
+	Formation saveFormation(Formation f);
+	Formation updateFormation(Formation f);
+	void deleteFormationparif(Long id);
+	Formation getFormation(Long id);
+	Page<Formation> getAllFormationparpage(int page,int size);
 	 Page<Formation> findByNomFormationContains(String nom,Pageable pageable);
 	 Page<Formation> findByDateDebutFormationContains(Date datedebut,Pageable pageable);
 	 Page<Formation> findByDateCertifFormationContains(Date datecertif,Pageable pageable);
