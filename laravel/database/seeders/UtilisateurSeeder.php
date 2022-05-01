7b05f3f29659e97e73641ace1ef55b8f7b64ddfd
nourhaine1 <ayadinourhaine@gmail.com>
2022-05-01 17:41:51 +0200

    updating seeders

--- laravel/database/seeders/UtilisateurSeeder.php
+++ laravel/database/seeders/UtilisateurSeeder.php
@@ -17,6 +17,6 @@ class UtilisateurSeeder extends Seeder
     public function run()
     {
        
-     Utilisateur::factory()->count(10)->create();
+     Utilisateur::factory()->count(20)->create();
     }
 }
