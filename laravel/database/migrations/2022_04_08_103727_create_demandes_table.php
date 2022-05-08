0bad956bd652d3790545e405bc92c52ae4749ca5
nourhaine1 <ayadinourhaine@gmail.com>
2022-05-08 13:54:47 +0200

    Updating migrations

--- laravel/database/migrations/2022_04_08_103727_create_demandes_table.php
+++ laravel/database/migrations/2022_04_08_103727_create_demandes_table.php
@@ -17,7 +17,7 @@ class CreateDemandesTable extends Migration
             $table->id();
             $table->date('date_demande');
             $table->foreignId('formation_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
-            $table->foreignId('utilisateur_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
            $table->timestamps();
         });
     }
