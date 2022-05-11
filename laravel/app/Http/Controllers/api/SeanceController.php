817afb8608ecce363501f72b4bb61069b894aabd
welhazitakwa <welhazitakwa@gmail.com>
2022-05-11 16:47:21 +0100

    correction store Seance

--- laravel/app/Http/Controllers/api/SeanceController.php
+++ laravel/app/Http/Controllers/api/SeanceController.php
@@ -26,18 +26,26 @@ class SeanceController extends Controller
     public function store(Request $request)
     {
        $newSeance = new Seance() ;
-      
-     $newSeance->nom_seance = $request->seance["nom_seance"];
+ //Seance::create($request->all());
+      /* $newSeance = Seance::create([
+             'nom_seance' => $request->nom_seance,
+             'date' => $request->date,
+             'temps_fin' => $request->temps_fin,
+             'temps_debut' => $request->temps_debut ,
+             'formation_id' => $request->formation_id 
+       ]);*/
+
+      $newSeance->nom_seance = $request->seance["nom_seance"];
       $newSeance->date = $request->seance["date"];
       $newSeance->temps_fin = $request->seance["temps_fin"];
-     // $newSeance->temps_debut = $request->seance["temps_debut"];
-     // $newSeance->temps_debut = $request->seance["formation_id"];
+      $newSeance->temps_debut = $request->seance["temps_debut"];
+      $newSeance->temps_debut = $request->seance["formation_id"];
      // $newSeance->temps_debut = $request->seance["temps_debut"];
      // $newSeance->nom_seance = $request->nom_seance;
-      //$newSeance->date = $request->date;
+     // $newSeance->date = $request->date;
      // $newSeance->temps_fin = $request->temps_fin;
-      $newSeance->temps_debut = $request->temps_debut;
-      $newSeance->temps_debut = $request->formation_id;       
+     // $newSeance->temps_debut = $request->temps_debut;
+     // $newSeance->temps_debut = $request->formation_id;  
        $newSeance->save() ;
        return $newSeance ; 
     }
@@ -62,14 +70,18 @@ class SeanceController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $existingSeance = Seance::find($id);
+        $existingSeance = Seance::findorfail($id);
         if ($existingSeance ) {
         $existingSeance->nom_seance=$request->seance["nom_seance"];
         $existingSeance->date=$request->seance["date"];
         $existingSeance->nom_seance=$request->seance["temps_fin"];
         $existingSeance->nom_seance=$request->seance["temps_debut"];
        // $existingSeance->formation_id=$request->seance["formation_id"];
-        $existingSeance->save();
+       // $existingSeance->nom_seance=$request->nom_seance;
+       // $existingSeance->date=$request->date;
+       // $existingSeance->nom_seance=$request->temps_fin;
+       // $existingSeance->nom_seance=$request->temps_debut;
+       // $existingSeance->save();
         return $existingSeance ;
         }
        else{ return "Seance non trouvé";}
@@ -83,7 +95,7 @@ class SeanceController extends Controller
      */
     public function destroy($id)
     {
-        $existingSeance = Seance::find($id);
+        $existingSeance = Seance::findorfail($id);
         if ($existingSeance ) {
             $existingSeance->delete();
             return "Seance supprimé" ;
