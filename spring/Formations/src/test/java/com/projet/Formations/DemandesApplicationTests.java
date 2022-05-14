50d8ee4dc1c11deb7dccf0322854bb94d961aec6
safedhaouadi <safadhaouadi319@gmail.com>
2022-05-14 12:03:04 +0100

    update

---
+++ spring/Formations/src/test/java/com/projet/Formations/DemandesApplicationTests.java
@@ -0,0 +1,16 @@
+//package com.projet.Formations;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//import com.projet.Formations.dao.DemandeRepository;
+//
+//
+//public class DemandesApplicationTests {
+//	@Autowired
+//	private DemandeRepository demandeRepository;
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//}
