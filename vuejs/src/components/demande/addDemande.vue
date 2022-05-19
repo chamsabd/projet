fb107b15ae8d20d147f02dfb1af1ee1b94bb5ff1
chamsabd <chamsabdelwahed42@gmail.com>
2022-05-19 11:04:37 +0100

    git chekout -f master

---
+++ vuejs/src/components/demande/addDemande.vue
@@ -0,0 +1,53 @@
+<template>
+    
+  <div>
+      <b-button pill variant="outline-warning" @click="senddemande()">send demande</b-button>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  data: function(){
+    return{
+ 
+    }
+  },
+  props:{
+    f:Object,
+  },
+  methods:{
+    senddemande() {
+      console.log(this.f);
+      // var now=new Date();
+      // var date=new Date(now.getFullYear(), now.getMonth(), now.getDate());
+      //   //  var fd = new FormData()
+        //   fd.append('formation_id',this.f.id);
+        //  fd.append('date_demande', date);
+        var demande={};
+        demande.formation_id=this.f.row.id;
+        demande.date_demande=(new Date()).toISOString().split('T')[0];
+        
+      axios(
+      {   url: 'http://127.0.0.1:8000/api/demandes/store',
+            method: 'post',
+            data: demande,
+          })
+      .then((response) => {
+        console.log(response);
+          
+        })
+        .catch((error) => {
+          console.log(error.response.data.message)
+          });
+    },
+    // deleteitm(id) {
+    //   axios
+    //     .delete("http://127.0.0.1:8000/api/demandes/" + id)
+    //     .then((response) => {
+    //       if (response.data == true) this.getdemandes();
+    //     })
+    //     .catch((error) => console.log(error.response));
+    // },
+  },
+};
+</script>
